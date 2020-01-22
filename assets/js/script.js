e0fe19f60569a39009254674544a0aae7e299df7
Jeffery Jones <yreffej94@gmail.com>
2020-01-21 17:40:33 -0800

    Progress on Matching Two Cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,31 @@
-var gameCardsEl = document.getElementById("game-cards");
+const gameCardsEl = document.getElementById("game-cards");
 gameCardsEl.addEventListener("click", handleClick);
+let firstCardClicked;
+let secondCardClicked;
+let firstCardClasses;
+let secondCardClasses;
 
 function handleClick(event) {
   console.log(event);
   if (event.target.className.indexOf("card-back") === -1) {
     return;
   }
-  event.target.classList.add("hidden");
+  if(!firstCardClicked){
+    firstCardClicked = event.target;
+    firstCardClicked.classList.add("hidden");
+    firstCardClasses = firstCardClicked.previousElementSibling.className;
+  }else {
+    secondCardClicked = event.target;
+    secondCardClicked.classList.add("hidden");
+    secondCardClasses = secondCardClicked.previousElementSibling.className;
+    if(firstCardClasses === secondCardClasses) {
+      console.log("The images match");
+    }else {
+      setTimeout(function () {
+                  firstCardClicked.classList.remove("hidden");
+                  secondCardClicked.classList.remove("hidden");} ,2000);
+
+    }
+  }
+
 }
